70115a468446c3d67bf43e863560cfc499f41ec7
Katia Alvarez <A01781097@tec.mx>
2025-09-28 00:20:42 -0600

    UPDATE: ruteo para visualizar operador

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,100 @@
-import { Admin, Resource, ShowGuesser} from "react-admin";
+import { Admin, Resource, ShowGuesser, CustomRoutes } from "react-admin";
+import { Route } from "react-router-dom";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate, PostShow } from "./posts";
-import { commentsList, commentsEdit, commentsCreate, commentsShow } from "./comments";
+import {
+  commentsList,
+  commentsEdit,
+  commentsCreate,
+  commentsShow,
+} from "./comments";
 import { albumList, albumEdit, albumCreate, albumShow } from "./albums";
 import { photoList, photoEdit, photoCreate, photoShow } from "./photos";
-import { todosList, todosEdit, todosCreate} from "./todos";
+import { todosList, todosEdit, todosCreate } from "./todos";
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
-import CommentIcon from '@mui/icons-material/Comment';
-import PhotoAlbumIcon from '@mui/icons-material/PhotoAlbum';
-import PhotoIcon from '@mui/icons-material/Photo';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import { Dashboard } from './Dashboard';
-import { authProvider } from './authProvider';
-import { i18nProvider } from './i18nProvider';
-import { MyLoginPage } from './MyLoginPage';
+import CommentIcon from "@mui/icons-material/Comment";
+import PhotoAlbumIcon from "@mui/icons-material/PhotoAlbum";
+import PhotoIcon from "@mui/icons-material/Photo";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import { Dashboard } from "./Dashboard";
+import { authProvider } from "./authProvider";
+import { i18nProvider } from "./i18nProvider";
+import { MyLoginPage } from "./MyLoginPage";
 import { notaCreate, notaEdit, notaList, notaShow } from "./notas";
-import { operadorPage } from './operador';
-import { UserCreateForm, UserEditForm } from './useUnique';
-import { CommentBankRounded} from "@mui/icons-material";
+import { OperatorPage } from "./operador";
+import { UserCreateForm, UserEditForm } from "./useUnique";
+import { CommentBankRounded } from "@mui/icons-material";
 
 export const App = () => (
-    <Admin dataProvider={dataProvider} dashboard={Dashboard} layout={Layout} authProvider={authProvider} i18nProvider={i18nProvider} loginPage={MyLoginPage}>
-        <Resource
-            name="users"
-            list={UserList}
-            create={UserCreateForm}
-            edit={UserEditForm}
-            show={ShowGuesser}
-            icon={UserIcon}
-        />
-        <Resource
-            name="posts"
-            list={PostList}
-            edit={PostEdit}
-            create={PostCreate}
-            show={PostShow}
-            icon={PostIcon}
-        />
-        <Resource
-            name="comments"
-            list={commentsList}
-            edit={commentsEdit}
-            create={commentsCreate}
-            show={commentsShow}
-            icon={CommentIcon}
-        />
-        <Resource
-            name="albums"
-            list={albumList}
-            edit={albumEdit}
-            create={albumCreate}
-            show={albumShow}
-            icon={PhotoAlbumIcon}
-        />
-        <Resource
-            name="photos"
-            list={photoList}
-            edit={photoEdit}
-            create={photoCreate}
-            show={photoShow}
-            icon={PhotoIcon}
-        />
-        <Resource
-            name="todos"
-            list={todosList}
-            edit={todosEdit}
-            create={todosCreate}
-            show={ShowGuesser}
-            icon={FormatListBulletedIcon}
-        />
-        <Resource 
-            name="notas"
-            list={notaList}
-            edit={notaEdit}
-            create={notaCreate}
-            show={notaShow}
-            icon={CommentBankRounded}
-        />
-    </Admin>
-)
-
-
+  <Admin
+    dataProvider={dataProvider}
+    dashboard={Dashboard}
+    layout={Layout}
+    authProvider={authProvider}
+    i18nProvider={i18nProvider}
+    loginPage={MyLoginPage}
+  >
+    <Resource
+      name="users"
+      list={UserList}
+      create={UserCreateForm}
+      edit={UserEditForm}
+      show={ShowGuesser}
+      icon={UserIcon}
+    />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      show={PostShow}
+      icon={PostIcon}
+    />
+    <Resource
+      name="comments"
+      list={commentsList}
+      edit={commentsEdit}
+      create={commentsCreate}
+      show={commentsShow}
+      icon={CommentIcon}
+    />
+    <Resource
+      name="albums"
+      list={albumList}
+      edit={albumEdit}
+      create={albumCreate}
+      show={albumShow}
+      icon={PhotoAlbumIcon}
+    />
+    <Resource
+      name="photos"
+      list={photoList}
+      edit={photoEdit}
+      create={photoCreate}
+      show={photoShow}
+      icon={PhotoIcon}
+    />
+    <Resource
+      name="todos"
+      list={todosList}
+      edit={todosEdit}
+      create={todosCreate}
+      show={ShowGuesser}
+      icon={FormatListBulletedIcon}
+    />
+    <Resource
+      name="notas"
+      list={notaList}
+      edit={notaEdit}
+      create={notaCreate}
+      show={notaShow}
+      icon={CommentBankRounded}
+    />
+    <CustomRoutes>
+      <Route path="/operator" element={<OperatorPage />}></Route>
+    </CustomRoutes>
+  </Admin>
+);
