2ff017b330dc477f4a9ed60af709a1404085d93c
Hector Lugo <hlugogabino@gmail.com>
2025-10-01 10:01:32 -0600

    feat: side bar ahora si completada y bien estilizada

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,27 +1,8 @@
-import { Card, CardContent, CardHeader, Box, Grid } from "@mui/material";
+import { Card, CardContent, CardHeader } from "@mui/material";
 
 export const Dashboard = () => (
-    <>
-        <Grid size={{ xs: 12, md: 6 }}>
-            <CardHeader title="PAGINA DE INICIO" />
-    
-            <Card  >
-                <CardHeader title="Integración de seguridad informática en redes y sistemas de software (Gpo 402)" />
-                <CardContent>Equipo 2 - Superhipermegaarchirequete Gran Equipo 2</CardContent>
-            </Card>
-        </Grid>
-        
-        <Box>
-            <h1>REPORTES RECIENTES</h1>
-            <Card sx={{ mt: 2,
-                    backgroundColor: "#e8e8e8"
-                }}>
-                
-                <CardHeader title="Integrantes" />
-            </Card>
-        </Box>
-        
-
-        
-    </>
+    <Card>
+        <CardHeader title="Integración de seguridad informática en redes y sistemas de software (Gpo 402)" />
+        <CardContent>Equipo 2 - Superhipermegaarchirequete Gran Equipo 2</CardContent>
+    </Card>
 );
